672a3022ccdef86fe2da7402ac3b14cf117e9e38
Zokirjon_coder2008 <72650484+Zokirjon-coder2008@users.noreply.github.com>
2021-07-24 11:16:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+alert('O`yinimizga xush kelibsiz!!!');
+confirm("O`ynashga rozimisiz?");
+
+
+function myFunction() {
+    document.getElementById("baraban").innerHTML = "YOU CLICKED ME!";
+}           
+        
+
+
+
+
